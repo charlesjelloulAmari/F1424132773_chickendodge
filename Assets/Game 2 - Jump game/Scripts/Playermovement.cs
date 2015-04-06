4581e26e15a1936caea33ea703e85f34bd1e7b0a
FranckPano <franck.pano@gmail.com>
2015-04-06 18:54:40 -0400

    Revert "Position et Jump foncitonne pas"
    This reverts commit e9e85206da672ab42e2659d5a7f44d04acb15396.

--- Assets/Game 2 - Jump game/Scripts/Playermovement.cs
+++ Assets/Game 2 - Jump game/Scripts/Playermovement.cs
@@ -6,13 +6,6 @@ public class Playermovement : MonoBehaviour
     public float movementSpeed = 5.0f;
     private bool isGrounded = false;
 
-	float distToGround;
-	
-	void Start(){
-		// get the distance to ground
-		distToGround = collider.bounds.extents.y;
-	}
-
 
     void Update() {
         rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0); //Set X and Z velocity to 0
@@ -20,7 +13,6 @@ public class Playermovement : MonoBehaviour
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed, 0, 0);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-			Debug.Log("OUI");
             Jump(); //Manual jumping
 
         }
@@ -37,13 +29,12 @@ public class Playermovement : MonoBehaviour
 			isGrounded = false;
 			rigidbody.velocity = new Vector3 (0, 0, 0);
 			rigidbody.AddForce (new Vector3 (0, 250, 0), ForceMode.Force);
-		}
+		}  
     }
 
     void FixedUpdate()
     {
-        //isGrounded = Physics.Raycast(transform.position, -Vector3.up, 1.0f);
-		isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.5f);
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, 1.0f);
         if (isGrounded)
         {
             //Jump(); //Automatic jumping
