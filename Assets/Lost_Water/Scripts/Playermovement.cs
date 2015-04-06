f4da697c76b2d8d38a264981c3b07d5cbdde37da
FranckPano <franck.pano@gmail.com>
2015-04-06 18:54:31 -0400

    Position et Jump foncitonne pas

--- Assets/Lost_Water/Scripts/Playermovement.cs
+++ Assets/Lost_Water/Scripts/Playermovement.cs
@@ -6,6 +6,13 @@ public class Playermovement : MonoBehaviour
     public float movementSpeed = 5.0f;
     private bool isGrounded = false;
 
+	float distToGround;
+	
+	void Start(){
+		// get the distance to ground
+		distToGround = collider.bounds.extents.y;
+	}
+
 
     void Update() {
         rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0); //Set X and Z velocity to 0
@@ -13,6 +20,7 @@ public class Playermovement : MonoBehaviour
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed, 0, 0);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+			Debug.Log("OUI");
             Jump(); //Manual jumping
 
         }
@@ -29,12 +37,13 @@ public class Playermovement : MonoBehaviour
 			isGrounded = false;
 			rigidbody.velocity = new Vector3 (0, 0, 0);
 			rigidbody.AddForce (new Vector3 (0, 250, 0), ForceMode.Force);
-		}  
+		}
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics.Raycast(transform.position, -Vector3.up, 1.0f);
+        //isGrounded = Physics.Raycast(transform.position, -Vector3.up, 1.0f);
+		isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.5f);
         if (isGrounded)
         {
             //Jump(); //Automatic jumping
